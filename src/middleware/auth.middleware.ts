92767fb50970b3af9e871740d820a7c8136af465
MrVyde <Mrvyde@outlook.com>
2026-04-24 14:12:01 +0100

    add optionalAuthenticate to allow both logged-in user and anonymous user access comment routes

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,29 +1,58 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/env";
-
-interface AuthRequest extends Request {
-  user?: jwt.JwtPayload;
-}
+import { UserTokenPayload } from "../types/user";
 
 export const authenticate = (
-  req: AuthRequest,
+  req: Request,
   res: Response,
   next: NextFunction
 ) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!authHeader?.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
   const token = authHeader.split(" ")[1];
 
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
   try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded as jwt.JwtPayload;
+    const decoded = jwt.verify(token, JWT_SECRET) as UserTokenPayload;
+
+    req.user = decoded; //works because of express.d.ts
     next();
   } catch {
     return res.status(401).json({ message: "Invalid token" });
   }
+};
+
+export const optionalAuthenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader?.startsWith("Bearer ")) {
+    return next(); // 👈 allow anonymous
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return next(); // 👈 still allow anonymous
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as UserTokenPayload;
+    req.user = decoded;
+  } catch {
+    // ignore invalid token, treat as anonymous
+  }
+
+  next();
 };
