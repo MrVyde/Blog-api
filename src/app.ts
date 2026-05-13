bc2ba99b288a42b2e24988c8c84500cbb6049004
MrVyde <Mrvyde@outlook.com>
2026-05-13 17:18:15 +0100

    feat(app): add CORS support and global error handler, standardize API routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import path from "node:path";
 import usersRoute from "./routes/auth.route";
 import postRoutes from "./routes/post.route";
 import commentRoutes from "./routes/comment.route";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
@@ -16,8 +18,16 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // Route
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use("/api/auth", usersRoute);
 app.use("/api/posts", postRoutes);
-app.use("/comments", commentRoutes);
+app.use("/api/comments", commentRoutes);
+
+app.use(errorHandler);
 
 export default app;
