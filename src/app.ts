44413a2caa991fea2911b9ac9fa4d16e584d23c6
MrVyde <Mrvyde@outlook.com>
2026-06-12 21:30:48 +0100

    fix(cors): update allowed origin, allowedHeaders &  methods

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,8 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5000"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use("/api/auth", usersRoute);
